da7895967cdb7bef72922ab8ea6c2c12185172f2
HOME <kaero313@naver.com>
2024-08-13 20:31:14 +0900

    각 api별 response 데이터 정리, 상태코드 추가, 성공/실패에 대한 분기

--- src/main/java/api/controller/apiController.java
+++ src/main/java/api/controller/apiController.java
@@ -33,18 +33,26 @@ public class apiController {
 
         member selectMember = apiService.selectMember(member);
 
-        try {
-            response.setContentType("text/html");
-            response.setCharacterEncoding("utf-8");
-            PrintWriter writer = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
 
+        try {
             if(selectMember != null){
-                writer.println("로그인 완료\n계정 이름은 " + selectMember.getName() + " 입니다");
+                response.setStatus(HttpServletResponse.SC_OK);
+
+                PrintWriter writer = response.getWriter();
+
+                JSONObject jsonobj = new JSONObject(selectMember);
+                writer.println(jsonobj);
+                writer.close();
             }else{
-                writer.println("아이디 또는 비밀번호를 확인해주세요");
+                response.setStatus(HttpServletResponse.SC_OK);
+
+                PrintWriter writer = response.getWriter();
+                writer.println("회원 조회 실패\n아이디 또는 비밀번호를 확인해주세요");
+                writer.close();
             }
 
-            writer.close();
         }catch (IOException e1){
             e1.printStackTrace();
         }
@@ -65,21 +73,31 @@ public class apiController {
             member.setPw(pw);
             member.setName(name);
 
-            response.setContentType("text/html");
+            response.setContentType("application/json");
             response.setCharacterEncoding("utf-8");
-            PrintWriter writer = response.getWriter();
 
             if(apiService.selectMemberIdCheck(id)){
+                response.setStatus(HttpServletResponse.SC_OK);
+
+                PrintWriter writer = response.getWriter();
                 writer.println("해당 아이디가 이미 존재합니다");
+                writer.close();
             }else{
                 if(apiService.insertMember(member)){
+                    response.setStatus(HttpServletResponse.SC_CREATED);
+
+                    PrintWriter writer = response.getWriter();
                     writer.println("회원가입 완료");
+                    writer.close();
                 }else{
+                    response.setStatus(HttpServletResponse.SC_OK);
+
+                    PrintWriter writer = response.getWriter();
                     writer.println("회원가입 실패\n다시 시도해주세요");
+                    writer.close();
                 }
             }
 
-            writer.close();
 
         }catch (IOException e1){
 
@@ -101,17 +119,23 @@ public class apiController {
             member.setPw(pw);
             member.setName(name);
 
-            response.setContentType("text/html");
+            response.setContentType("application/json");
             response.setCharacterEncoding("utf-8");
-            PrintWriter writer = response.getWriter();
 
             if(apiService.updateMember(member)){
+                response.setStatus(HttpServletResponse.SC_OK);
+
+                PrintWriter writer = response.getWriter();
                 writer.println("회원정보 변경 완료");
+                writer.close();
             }else{
-                writer.println("회원정보 변경 실패\n다시 시도해주세요");
+                response.setStatus(HttpServletResponse.SC_OK);
+
+                PrintWriter writer = response.getWriter();
+                writer.println("회원정보 변경 실패\n회원 정보를 확인 후에 다시 시도해주세요");
+                writer.close();
             }
 
-            writer.close();
 
         }catch (IOException e1){
 
@@ -127,23 +151,27 @@ public class apiController {
             JSONObject jsonobj = new JSONObject(request1);
             String id = jsonobj.getString("id");
             String pw = jsonobj.getString("pw");
-            String name = jsonobj.getString("name");
 
             member.setId(id);
             member.setPw(pw);
-            member.setName(name);
 
-            response.setContentType("text/html");
+            response.setContentType("application/json");
             response.setCharacterEncoding("utf-8");
-            PrintWriter writer = response.getWriter();
 
             if(apiService.deleteMember(member)){
+                response.setStatus(HttpServletResponse.SC_OK);
+
+                PrintWriter writer = response.getWriter();
                 writer.println("회원 삭제 완료");
+                writer.close();
             }else{
-                writer.println("회원 삭제 실패\n다시 시도해주세요");
+                response.setStatus(HttpServletResponse.SC_OK);
+
+                PrintWriter writer = response.getWriter();
+                writer.println("회원 삭제 실패\n회원 정보를 확인 후에 다시 시도해주세요");
+                writer.close();
             }
 
-            writer.close();
 
         }catch (IOException e1){
 
